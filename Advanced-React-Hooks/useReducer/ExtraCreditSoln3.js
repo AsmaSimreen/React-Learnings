8a43aff4164c9730c53cec9eeefbdcbfa7fcc81d
AsmaSimreen <asmasimreen1001@gmail.com>
2022-10-16 21:03:13 +0530

    Extra Credit Solutions

---
+++ Advanced-React-Hooks/useReducer/ExtraCreditSoln3.js
@@ -0,0 +1,21 @@
+//Object or Function
+
+import React from 'react'
+
+const countReducer = (state, action) => ({ 
+  ...state, 
+  ...(typeof action === 'function' ? action(state):action), 
+})
+
+function Counter({ initialCount = 0, step = 1 }) {
+  const [state, setState] = React.useReducer(countReducer, { count: initialCount });
+  const { count } = state
+  const increment = () => setState({ count: currentState.count + step })
+  return <button onClick={increment}>{count}</button>
+}
+
+function App() {
+  return <Counter />
+}
+
+export default App
